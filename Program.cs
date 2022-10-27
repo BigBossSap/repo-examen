cd14f1210ac1a3acdf1ef82149b97a699f88b245
SOC <SOC@imontilivi.cat>
2022-10-27 19:31:39 +0200

    canvis

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//DV
+
+int hores;
+double preuHora, irpf, salarioBruto, retencion, sueldoNeto;
+
+
+//Valores
+
+Console.WriteLine("Horas trabajadas: ");
+hores = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Precio por hora: ");
+preuHora = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("% de IRPF: ");
+irpf = Convert.ToDouble(Console.ReadLine());
+
+//Algoritmos
+
+salarioBruto = hores * preuHora;
+
+retencion = salarioBruto * irpf/100;
+
+sueldoNeto = salarioBruto - retencion;
+
+Console.WriteLine("Salario Bruto: " + salarioBruto);
+Console.WriteLine("Retención: " + retencion);
+Console.WriteLine("Salario Neto: " + sueldoNeto);
+
+
+
+
+
+
+
+
+
+
+
+
+
